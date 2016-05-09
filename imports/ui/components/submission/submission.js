c881d6553dbfd09085733a16f45bdf532fe82a7e
NickBittar <Nickcb7@gmail.com>
2016-05-08 20:07:11 -0400

    Fixed header and footer.  Made them show up, and on create page, footer properly sticks to the bottom of the page.

--- imports/ui/components/submission/submission.js
+++ imports/ui/components/submission/submission.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Events } from '/imports/api/events.js';
+import '../navbar/navbar.js';
 import './submission.html';
+import '/lib/TimeList.js';
 
 /* GLOBALS */
 var time,
@@ -30,8 +32,17 @@ Template.submission.events({
     }
     drawline(event);
   },
-  'click .floating-submit-button'() {
-    console.info(timeList.export());
+  'click .submission-button'(event) {
+    event.preventDefault();
+    let name = $('#name').val().trim();
+    if(name.length < 1)
+    {
+      alert("NEED A NAME!");
+      return false;
+    }
+    let JSONexport = timeList.export(name);
+    console.info(JSONexport);
+    return JSONexport;
   },
 });
 
@@ -60,135 +71,7 @@ Template.submission.helpers({
 
 
 
-class TimeList {
-  constructor(startDate) {
-    this.days = [];
-    this.startDate = startDate; //This is a String, not a Date object
-  }
 
-  addTime(dayIndex, time)
-  {
-    // If there is no data for that day, create it
-    if(this.days[dayIndex] === undefined)
-    {
-      this.days[dayIndex] = [];
-    }
-    this.days[dayIndex].push(time);
-  }
-
-  removeTime(dayIndex, time)
-  {
-    let timeIndex = this.days[dayIndex].indexOf(time);
-    if(timeIndex === -1)
-    {
-      return false;
-    }
-    this.days[dayIndex].splice(timeIndex, 1);
-  }
-
-  getTimes(dayIndex)
-  {
-    return this.days[dayIndex];
-  }
-
-  export() {
-    let currDay;
-    let data = { name: 'Nick', times: [], };
-    const dayList = $('#day-list')[0];
-
-    for(i in this.days)  // For each day
-    {
-      // Sets the current day to correct day based on the index of the array we are at
-      currDay = new Date(dayList.children[i].firstElementChild.innerText);
-
-      // Goes through each time in the current day
-      for(j in this.days[i])  // For each time block in the day
-      {
-        let [startMin, endMin] = this.getMinutes(this.days[i][j]);
-
-        let startTime = new Date(currDay.getTime()),
-            endTime   = new Date(currDay.getTime());
-
-        startTime.setMinutes(startMin);
-        endTime.setMinutes(endMin);
-
-        let newData = {start: startTime, end: endTime,};
-
-        data.times.push(newData);
-      }
-    }
-    console.info('Export Finished.');
-    return data;
-  }
-
-  import(data)
-  {
-    this.startDate = $('#start-date').text();
-    const dayList = $('#day-list')[0];
-    const dayCount = dayList.children.length;
-    let dayElements = [];
-    for(let i = 0; i < dayCount; i += 1)
-    {
-      dayElements[i] = dayList.children[i].lastElementChild.firstElementChild;
-    }
-    const times = data.times;
-    const minutesInDay = 60*24;
-    const dayWidth = dayElements[0].clientWidth;
-    for(let i = 0; i < times.length; i += 1)
-    {
-      if(times[i] === undefined) { continue; }
-
-      let startTime = new Date(times[i].start);
-      let endTime = new Date(times[i].end);
-
-      let dayIndex = startTime.getDate() - new Date(this.startDate).getDate();
-
-      startTime = startTime.getHours()*60 + startTime.getMinutes();
-      endTime = endTime.getHours()*60 + endTime.getMinutes();
-
-      let div = document.createElement('div');
-      div.className = 'line good';
-      div.style.left = (startTime/minutesInDay)*100 + '%';
-      div.style.width = ((endTime-startTime)/minutesInDay)*100 + '%';
-      this.addTime(dayIndex, div);
-
-      dayElements[dayIndex].appendChild(div);
-      updateAllTimes(dayIndex);
-    }
-
-    console.info('Import finished.');
-  }
-
-  getMinutes(div)
-  {
-    let space = $('.time-box')[0].getBoundingClientRect();
-  	let rect = div.getBoundingClientRect();	// the div's rectangle position
-  	let timeStart, timeEnd;	// Will hold the time in minutes
-  	let l = rect.left-space.left;			// left side of div to left side of screen
-  	let r = rect.right-space.left;			// right side of div to left side of screen
-  	let w = $('.time-box')[0].clientWidth;	// screen width
-
-  	//Fix edge case errors that result in -1:58AM on the left and 12:01PM on right
-  	if(l < 0) { l = 0; }
-    if(r < 0) { r = 0; }
-  	if(r > w) { r = w; }
-    if(l > w) { l = w; }
-
-  	/* Set Ratios */
-  	let rl = l/w;						// Ratio of left-side of div to screen width
-  	let rr = r/w;						// Ratio of right-side of div to screen width
-
-    const minutesInDay = 24 * 60;
-    timeStart = rl * minutesInDay;
-    timeEnd = rr * minutesInDay;
-
-    // To fix the end of the day being 12:00PM when it should be 11:59PM
-    if(l === w) { timeStart -= 1; }
-    if(r === w) { timeEnd -= 1; }
-
-    return [timeStart, timeEnd];
-  }
-}
 
 
 
